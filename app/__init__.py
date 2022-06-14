d233934dde2ae2369e800aeed56397afeb9f88f1
harry <harrisongitau3@gmail.com>
2022-06-14 17:55:28 +0300

    Feat: Handle bad URL error and db error

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 
+
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
---
+++ app/errors.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import render_template
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('errors/404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
