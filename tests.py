c271bdac16b3298a0ce9e58e912c69d251d242a4
harry <harrisongitau3@gmail.com>
2022-06-15 09:44:45 +0300

    test user model

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+from app.models import User
+from app import app, db
+
+
+class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_password_hashing(self):
+        u = User(username='susan')
+        u.set_password('cat')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
